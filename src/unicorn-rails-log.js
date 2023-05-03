069187b4390598b7f7fe25d1a864fb0ff754f3d4
SweetPPro <sweetppro@users.noreply.github.com>
2023-05-03 12:25:01 +0200

    improved definitions

--- src/unicorn-rails-log.js
+++ src/unicorn-rails-log.js
@@ -7,7 +7,7 @@ Author: Russell Gray <rgray@sweetpproductions.com>
 
 export default function (hljs) {
   var UNICORN_KEYWORDS = {
-    keyword: 'Rendering Rendered Completed Started POST Processing by Redirected to Delivered mail ' + 'Performed GET in text template for at as HTML JS CSS within',
+    keyword: 'Rendering Rendered Completed Started POST Processing by Redirected to Delivered mail Performing layout Filter chain halted Enqueued ' + 'Performed GET in text template for at as HTML JS CSS within',
     literal: 'true false nil'
   };
 

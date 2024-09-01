6055e32c7c7a24cc00fc5d774000fa8bfcb8101a
SweetPPro <sweetppro@users.noreply.github.com>
2024-09-01 21:25:44 +0200

    syntax improvements

--- src/unicorn-rails-log.js
+++ src/unicorn-rails-log.js
@@ -7,94 +7,131 @@ Author: Russell Gray <rgray@sweetpproductions.com>
 
 export default function (hljs) {
   var UNICORN_KEYWORDS = {
-    keyword: 'Rendering Rendered Completed Started POST Processing by Redirected to Delivered mail Performing layout Filter chain halted Enqueued ' + 'Performed GET in text template for at as HTML JS CSS within from enqueued with',
-    literal: 'true false nil'
+    keyword:
+      "rendering rendered completed started post processing by redirected to delivered mail performing layout filter chain halted enqueued performed get in text template for at as HTML JS CSS PNG JPG within from enqueued with redirected or",
+    literal: "true false nil",
   };
 
   var STRING = {
-    className: 'string',
+    scope: "string",
     contains: [hljs.BACKSLASH_ESCAPE],
-    variants: [{
+    variants: [
+      {
         begin: /'/,
-        end: /'|\n/
+        end: /'|\n/,
       },
       {
         begin: /"/,
-        end: /"|\n/
+        end: /"|\n/,
       },
       {
         begin: /`/,
-        end: /`|'|\n/
-      }
-    ]
+        end: /`|'|\n/,
+      },
+    ],
   };
 
   var COMMENT = {
-    className: 'comment',
+    scope: "comment",
     variants: [
-      hljs.COMMENT('#', '$'), {
-
+      hljs.COMMENT("#", "$"),
+      {
         begin: /\[/,
-        end: /\]|\n/
-      }
-    ]
+        end: /\]|\n/,
+      },
+    ],
   };
 
   var CLASS = {
-    className: 'built_in',
+    scope: "built_in",
     begin: "\\b\\w*::\\S*",
-    relevance: 9
-  }
+    relevance: 10,
+  };
+
+  var LINK = {
+    scope: "link",
+    variants: [
+      {
+        begin: "http(s?):",
+        end: "\\S",
+        relevance: 10,
+      },
+    ],
+  };
 
   var FILE = {
-    className: 'name',
-    variants: [{
+    scope: "name",
+    variants: [
+      {
         begin: "\\b\\w*(\\/)\\S*",
-        relevance: 10
-    },
-    {
-      begin: "\\S*#\\S*",
-      relevance: 10
-    }
-    ]
-  }
+        relevance: 10,
+      },
+      {
+        begin: "\\S*#\\S*",
+        relevance: 10,
+      },
+    ],
+  };
 
   var SYMBOL = {
-    className: 'symbol',
-    variants: [{
-        begin: hljs.UNDERSCORE_IDENT_RE + '(\\!|\\?)?:',
-        relevance: 0
+    scope: "symbol",
+    variants: [
+      {
+        begin: ":\\S",
+        end: "\\s",
+        relevance: 0,
+      },
+      {
+        begin: e.UNDERSCORE_IDENT_RE + "(\\!|\\?)?:",
+        relevance: 0,
       },
       {
         begin: "(\\*).*?(?=:)",
-        relevance: 0
+        relevance: 0,
       },
       {
-        begin: ':(?!\\s)',
-        contains: [STRING],
-        relevance: 0
+        begin: ":(?!\\s)",
+        contains: [n],
+        relevance: 0,
       },
       {
-        begin: '^\\S*?:',
-        relevance: 0
+        begin: "^\\S*?:",
+        relevance: 0,
       },
       {
-        begin: 'Job ID:',
-        relevance: 0
-      }
-    ]
+        begin: "Job ID",
+        relevance: 0,
+      },
+    ],
+  };
+
+  var UUID = {
+    scope: "literal",
+    variants: [
+      {
+        begin: "[a-zA-Z0-9]*?-",
+        end: "\\s",
+        relevance: 0,
+      },
+    ],
+  };
+
+  var NUMBER = {
+    scope: "number",
+    variants: [
+      {
+        begin: e.NUMBER_RE,
+        end: "\\s",
+        relevance: 0,
+      },
+    ],
   };
 
   return {
-    name: 'Unicorn Rails log',
+    name: "Unicorn Rails log",
+    case_insensitive: true,
     keywords: UNICORN_KEYWORDS,
     illegal: /\/\*/,
-    contains: [
-      STRING,
-      COMMENT,
-      CLASS,
-      FILE,
-      SYMBOL
-    ]
+    contains: [STRING, COMMENT, CLASS, LINK, FILE, SYMBOL, UUID, NUMBER],
   };
 }
